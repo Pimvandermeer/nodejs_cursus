c8f6eea958893c7e51666a930356de8a9b8c05fc
cyclerunner1 <meertrompet@gmail.com>
2020-03-04 20:20:07 +0100

    till lesson 16

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const replaceTemplate = (temp, product) => {
   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
   if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
@@ -54,10 +55,10 @@ const dataObj = JSON.parse(data);
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   //Overview page
-  if(pathName === '/' || pathName === '/overview') {
+  if(pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {'Content-type': 'text/html'});
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard , el)).join('');
@@ -66,11 +67,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
   //Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT!');
+} else if (pathname === '/product') {
+  res.writeHead(200, {'Content-type': 'text/html'});
+  const product = dataObj[query.id];
+  const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
   //API
-  } else if (pathName === '/api') {
+} else if (pathname === '/api') {
       res.writeHead(200, {'Content-type': 'application/json'});
       res.end(data);
 
